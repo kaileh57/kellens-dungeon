bd9a8a3e283a53e1a9c42dce1c2e3a1a12f3d64b
kaileh57 <kellenhe@hotmail.com>
2023-05-12 12:47:14 -0700

    added green

--- testing_and_debugging/testing.gd
+++ testing_and_debugging/testing.gd
@@ -5,6 +5,12 @@ extends Node2D
 # spawn in the correct character
 func _ready():
 		var character = ResourceLoader.load($"/root/GlobalSettings".character_path).instantiate()
-		add_child(character)
+		$Characters.add_child(character)
+		character.name = $"/root/GlobalSettings".character_name
+		character = ResourceLoader.load("res://characters/blue/blue.tscn").instantiate()
+		$Characters.add_child(character)
+		character.name = $"/root/GlobalSettings".character_name
+		character = ResourceLoader.load("res://characters/green/green.tscn").instantiate()
+		$Characters.add_child(character)
 		character.name = $"/root/GlobalSettings".character_name
 
