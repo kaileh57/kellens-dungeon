d323192707f339a85a9f789eda19ce2b74cc1f0d
kaileh57 <kellenhe@hotmail.com>
2023-07-07 12:13:45 -0700

    .

--- level_generation/level_generator.gd
+++ level_generation/level_generator.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 #Rooms sorted by prefab
-const SPAWN_ROOMS = []
+const SPAWN_ROOMS = [preload("res://rooms/default/easy_rooms/easy1.tscn")]
 const EASY_ROOMS = [preload("res://rooms/default/easy_rooms/easy1.tscn")]
 
 const HUB_ROOMS = [preload("res://rooms/default/hub_rooms/hub1.tscn")]
@@ -23,13 +23,40 @@ const BOSS_ROOMS = []
 @export var hard_chance := 10
 @export var loot_rooms := 2
 
+#placement settings
+var tile_size := 16
+@export var hub_varience := 0
+@export var spawn_varience := 20
+
+func _ready():
+	generate_level()
+
 func generate_level():
 	place_hub_room()
-	
+	place_spawn_room()
 
 
 func place_hub_room():
 	randomize()
 	var room = HUB_ROOMS[randi() % HUB_ROOMS.size()].instantiate()
+	print(room)
 	rooms.add_child(room)
+	room.position.x = randi_range(-hub_varience, hub_varience)*tile_size
+	room.position.y = randi_range(-hub_varience, hub_varience)*tile_size
 	room.name = "Hub"
+
+func place_spawn_room():
+	randomize()
+	var room = SPAWN_ROOMS[randi() % SPAWN_ROOMS.size()].instantiate()
+	print(room)
+	rooms.add_child(room)
+	randomize()
+	room.position.x = randi_range(-spawn_varience, spawn_varience)*tile_size
+	room.position.y = randi_range(-spawn_varience, spawn_varience)*tile_size
+	await get_tree().process_frame
+	print(room.find_child("RoomShape").has_overlapping_areas())
+	while room.find_child("RoomShape").has_overlapping_areas():
+		randomize()
+		room.position.x = randi_range(-spawn_varience, spawn_varience)*tile_size
+		room.position.y = randi_range(-spawn_varience, spawn_varience)*tile_size
+	room.name = "Spawn"
---
+++ rooms/default/room_base/room_base.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func check_collision() -> void:
+	pass
